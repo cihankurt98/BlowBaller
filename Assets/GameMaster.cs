50e14b7016d1fe7e26d6458b61d505eb205c559f
Cihan <cihankurt1998@gmail.com>
2018-05-18 13:53:07 +0200

    UI inorde, gamemaster begin
    UI is helemaal in orde momenteel. GameMaster toegevoegd om alle variabelen, die te maken hebben met de moeilijkheid, te kunnen aanpassen. (eis van Jack en Bart voor GTO herkansing)

---
+++ Assets/GameMaster.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class GameMaster : MonoBehaviour
+{
+    [Header("Ball settings")]
+    public Vector3 ballThrowSpeed;
+    public Vector3 ballGravity;
+    [Header("Meter settings")]
+    public float arrowSpeed;
+    public static GameMaster instance;
+
+    void Awake()
+    {
+        if (instance != null)
+        {
+            Destroy(instance);
+        }
+        else
+        {
+            instance = this;
+        }
+
+        DontDestroyOnLoad(this);
+    }
+
+
+
+    void Start()
+    {
+
+    }
+
+
+}
